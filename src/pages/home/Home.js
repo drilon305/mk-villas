854a6add408605185a7d5e7b5d8348b45c53ff82
drilon305 <drilonsulejmani305@gmail.com>
2022-07-03 18:50:02 +0200

    added featuredproperties

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import Header from '../../components/header/Header'
 import './home.css'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
         <h1 className='homeTitle'>Browse by property type</h1>
         <PropertyList />
         <h1 className='homeTitle'>Home guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
