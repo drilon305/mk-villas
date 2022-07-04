dd4de8dae3a3c51de6cd35b96e7c6b0274406073
drilon305 <drilonsulejmani305@gmail.com>
2022-07-04 19:54:18 +0200

    designed footer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -61,10 +61,9 @@ const Header = ({ type }) => {
         </div>
         { type !== 'list' &&
           <> 
-          <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+          <h1 className="headerTitle">Find your next stay</h1>
         <p className="headerDescription">
-          Get reward for your travels - unlock instant savings of 10% or more
-          with a free Booking.com account
+        Search deals on hotels, homes, and much more...
         </p>
         <button className="headerBtn">Sign in / Register</button>
         <div className="headerSearch">
--- src/components/propertyList/PropertyList.js
+++ src/components/propertyList/PropertyList.js
@@ -11,7 +11,7 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg" alt='' />
+            <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg" alt='' className="pListImg" />
             <div className="pListTitles">
         <h1>Apartments</h1>
         <h2>223 hotels</h2>
